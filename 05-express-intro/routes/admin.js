f91cae0335d64f33377ce341520ef64c8227b453
sumitsasidharan <s.sumit196@gmail.com>
2025-01-03 20:43:49 +0530

    confict resolved

--- 05-express-intro/routes/admin.js
+++ 05-express-intro/routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const path = require('path');
+
 router.get('/add-product', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
 });
