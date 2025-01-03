07536e3ff21616585fd5fb965969350b4a8281e7
sumitsasidharan <s.sumit196@gmail.com>
2025-01-03 20:40:45 +0530

    desktop 3rd jan 05..

--- 05-express-intro/app.js
+++ 05-express-intro/app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -13,7 +14,8 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+  // res.status(404).send('<h1>Page not found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 })
 
 // const server = http.createServer(app);
