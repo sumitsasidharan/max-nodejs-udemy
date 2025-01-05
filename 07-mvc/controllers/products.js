189a65985e0d6562e580ebe665c868d64123181d
sumitsasidharan <s.sumit196@gmail.com>
2025-01-05 13:16:24 +0530

    mvc

--- 07-mvc/controllers/products.js
+++ 07-mvc/controllers/products.js
@@ -1,4 +1,4 @@
-
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -6,6 +6,28 @@ exports.getAddProduct = (req, res, next) => {
     path: '/admin/add-product',
     formsCSS: true,
     productCSS: true,
-    activeAddProduct: true
+    activeAddProduct: true,
   });
-}
+};
+
+exports.postAddProduct = (req, res, next) => {
+  // products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true,
+    });
+  });
+
+  
+};
--- 07-mvc/routes/admin.js
+++ 07-mvc/routes/admin.js
@@ -7,16 +7,12 @@ const router = express.Router();
 
 const productsController = require('../controllers/products');
 
-const products = [];
+
 
 // /admin/add-product => GET
-router.get('/add-product', productsController);
+router.get('/add-product', productsController.getAddProduct);
 
 // /admin/add-product => POST
-router.post('/add-product', (req, res, next) => {
-  products.push({ title: req.body.title });
-  res.redirect('/');
-});
+router.post('/add-product', productsController.postAddProduct);
 
-exports.routes = router;
-exports.products = products;
+module.exports = router;
