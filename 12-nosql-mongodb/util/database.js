520f6e3475225bd6c793a80e328a3c7569381c60
sumitsasidharan <s.sumit196@gmail.com>
2025-01-13 20:11:15 +0530

    12 mongodb...

--- 12-nosql-mongodb/util/database.js
+++ 12-nosql-mongodb/util/database.js
@@ -1,8 +1,32 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+// const { MongoClient } = require('mongodb');
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const url =
+  'mongodb+srv://sumit:ftUpNK1ee4c7Top0@cluster0.l2iur.mongodb.net/testdb?retryWrites=true&w=majority&appName=Cluster0';
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(url)
+    .then((client) => {
+      console.log('Connected!!');
+      _db = client.db('testdb');
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw 'No database found!';
+};
+
+// module.exports = mongoConnect;
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
