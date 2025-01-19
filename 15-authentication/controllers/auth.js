0171a09b8799470ef8838834c3609bf99b52b993
sumitsasidharan <s.sumit196@gmail.com>
2025-01-19 15:24:25 +0530

    started validation...

--- 15-authentication/controllers/auth.js
+++ 15-authentication/controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require('crypto'); // built-in node module
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
+const { validationResult } = require('express-validator/check');
 
 const User = require('../models/user');
 
@@ -45,12 +46,23 @@ exports.getSignup = (req, res, next) => {
     path: '/signup',
     pageTitle: 'Signup',
     errorMessage: message,
-    // isAuthenticated: false,
+    oldInput: { email: '', password: '', confirmPassword: '' },
+    validationErrors: [],
   });
 };
 
 exports.postLogin = (req, res, next) => {
   const { email, password } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // console.log(errors.array());
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+    });
+  }
+
   User.findOne({ email: email })
     .then((user) => {
       // check if user exists
@@ -87,6 +99,23 @@ exports.postLogin = (req, res, next) => {
 
 exports.postSignup = (req, res, next) => {
   const { email, password, confirmPassword } = req.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    // console.log(errors.array());
+    return res.status(422).render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password,
+        confirmPassword: confirmPassword,
+      },
+      validationErrors: errors.array(),
+    });
+  }
+
   // first check if user / email already exists
   User.findOne({ email: email })
     .then((userDoc) => {
---
+++ README.md
@@ -0,0 +1,5 @@
+## Packages Used for Validation.
+
+### validator express-validator
+
+Used for form validation. Used in auth routes & controller.
