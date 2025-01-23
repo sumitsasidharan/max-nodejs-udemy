efad83befdac7a1c16ea93d34b0976bd72c9f79d
sumitsasidharan <s.sumit196@gmail.com>
2025-01-23 21:00:33 +0530

    added aync await

--- 24-25-rest-all/controllers/feed.js
+++ 24-25-rest-all/controllers/feed.js
@@ -15,6 +15,7 @@ exports.getPosts = (req, res, next) => {
     .then((count) => {
       totalItems = count;
       return Post.find()
+        .populate('creator')
         .skip((currentPage - 1) * perPage)
         .limit(perPage);
     })
