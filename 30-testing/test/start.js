44d2c9c3262e999ca771af385210d800bbec923c
sumitsasidharan <s.sumit196@gmail.com>
2025-01-28 23:10:35 +0530

    testing...

---
+++ 30-testing/test/start.js
@@ -0,0 +1,16 @@
+// const { expect } = require('chai');
+import { expect } from 'chai';
+
+it('should add numbers correctly', function () {
+  const num1 = 2;
+  const num2 = 3;
+  // expect returns an object
+  expect(num1 + num2).to.equal(5);
+});
+
+it('should not give a result of 6', function () {
+  const num1 = 2;
+  const num2 = 3;
+  // expect returns an object
+  expect(num1 + num2).not.to.equal(6);
+});
